ebed85a26e1da10b64cd540461e34f4c24627d13
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-06-20 18:16:40 -0500

    Section 4 complete, all notes, not code

---
+++ Section-4-Problem-Solving-Approach/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// WHAT IS AN ALGORITHM?!!
+// A process or set of steps to accomplish a certain task
+
+// Why do I need to know this?
+// Almost everything that you do in programming involves some kind of algorithm!
+// It's the foundation for being a successful problem solver and developer
+
+// How do you improve at this stuff?
+// 1. Devise a plan for solving problems
+// 2. Master common problem solving patterns
+
+// Problem Solving
+// 1. Understand the Problem
+// 2. Explore concrete examples
+// 3. Break it down
+// 4. Solve/Simplify
+// 5. Look back and refactor
+
+// UNDERSTAND THE PROBLEM ---
+// Can I restate the problem in my own words?
+// What are the inputs that go into the problem?
+// What are the outputs that should come from the solution to the problem?
+// Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem? (You may not be able to answer this question until you set about solving the problem. That's okay; it's still worth considering the question at this early stage.)
+// How should I label the important pieces of data that are a part of the problem?
+// How should I label the important pieces of data that are a part of the problem?
+
+// EXPLORE CONCRETE EXAMPLES ---
+// Coming up with examples can help you understand the problem better
+// Examples also provide sanity checks that your eventual solution works how it should.
+// Start with simple examples
+// Progress to more complex examples
+// Explore examples with empty inputs
+// Explore examples with invalid inputs
+
+// BREAK IT DOWN ---
+// Explicitly write out the steps you need to take
+
+// SOLVE / SIMPLIFY ---
+// Find the core difficulty in what you are trying to do
+// Temporarily ignore that difficulty
+// Write a simplified solution
+// Then incorporate that difficulty back in
+
+// LOOK BACK AND REFACTOR --
+// Can you check the result?
+// Can you derive the result differently?
+// Can you understand it at a glance?
+// Can you use the result or mehod for some other problem?
+// Can you improve the performance of your solutions?
+// Can you think of other ways to refactor?
+// How have other people solved this problem?
