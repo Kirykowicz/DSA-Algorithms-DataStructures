70c61ba895e3793a57284a2293c2a9b0e87966b9
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-06-20 12:25:18 -0500

    notes/code for section 3 completed

---
+++ Section-3-Analizing-performance-of-arrays-and-objects/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// When to use Objects?
+// 1. When you don't need order
+// 2. When you need fast access / insertion and removal
+
+// Big O of Objects
+// Insertion - O(1)
+// Removal - O(1)
+// Searching - O(n)
+// Access - O(1)
+
+// Big O of Object Methods
+// Object.keys - O(n)
+// Object.values - O(n)
+// Object.entries - O(n)
+// hasOwnProperty - O(1)
+
+// When to use Arrays?
+// 1. When you need order
+// 2. When you need fast access / insertion and removal (sort of....)
+
+// Big O of Arrays
+// Insertion - It depends .... (beginning or end of array?)
+// Removal - It depends .... (beginning or end of array?)
+// Searching - O(n)
+// Access - O(1)
+
+// Big O of Array Operations
+// push - O(1)
+// pop - O(1)
+// shift - O(n)
+// unshift - O(n)
+// concat - O(n)
+// slice - O(n)
+// splice - O(n)
+// sort - O(n*logn)
+// forEach/map/filter/reduce/etc. - O(n)
