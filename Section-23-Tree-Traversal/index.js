6b8bc8806dc22e154398ed0aac4446bf7bb5cb4a
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-11-12 15:52:57 -0600

    tree traversal complete

--- Section-23-Tree-Traversal/index.js
+++ Section-23-Tree-Traversal/index.js
@@ -77,11 +77,46 @@ class BinarySearchTree {
   DFSPreOrder() {
     var data = [];
     function traverse(node) {
-      data.push(node);
+      data.push(node.value);
       if (node.left) traverse(node.left);
       if (node.right) traverse(node.right);
     }
     traverse(this.root);
     return data;
   }
+
+  DFSPostOrder() {
+    var data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DFSInOrder() {
+    var data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
+
+const tree = new BinarySearchTree();
+
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
